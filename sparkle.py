13a5abb16aefa90e6b842493a252459eb4941fdf
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-08-19 20:31:38 -0500

    Update sparkle.py
    Up to date with half of episode 3. Program can now do simple calculations involving numbers!

--- sparkle.py
+++ sparkle.py
@@ -37,7 +37,11 @@ class IllegalCharError(Error):
 # Error in the parsing process
 class InvalidSyntaxError(Error):
     def __init__(self,pos_start,pos_end, details=' '):
-        super().__init__(pos_start,pos_end,"Invalid Syntax",details)        
+        super().__init__(pos_start,pos_end,"Invalid Syntax",details) 
+# You CANNOT divide by 0
+class RTError(Error):
+    def __init__(self,pos_start,pos_end, details=' '):
+        super().__init__(pos_start,pos_end,"Runtime Error",details)                
 
 #################################
 # ------------------ POSITION 
@@ -176,6 +180,10 @@ class Lexer:
 class NumberNode:
     def __init__(self,tok):
         self.tok = tok
+
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
     def __repr__(self):
         return f'{self.tok}'
     
@@ -187,6 +195,11 @@ class BinOpNode:
         self.left_node = left_node
         self.op_tok = op_tok
         self.right_node = right_node
+
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
+
     
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
@@ -195,6 +208,9 @@ class UnaryOpNode:
     def __init__(self, op_tok, node) :
         self.op_tok = op_tok
         self.node = node
+
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
 
     def __repr__(self):  
         return f'({self.op_tok}, {self.node})'  
@@ -305,7 +321,121 @@ class Parser:
             left = BinOpNode(left, op_tok, right)
 
         return res.success(left)
+#################################
+# ------------------ RUNTIME RESULT 
+##################################
 
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error:
+            self.error = res.error  
+        return res.value
+    
+    def success(self, value):
+        self.value = value
+        return self
+    
+    def failure(self, error):
+        self.error = error
+        return self
+
+#################################
+# ------------------ VALUES 
+##################################
+
+class Number:
+    def __init__(self,value):
+        self.value = value
+        self.set_pos()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value), None
+
+    def subtracted_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value), None    
+    
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value), None
+
+    def dived_by(self, other):
+        if isinstance(other, Number):
+            if other.value == 0:
+                return None, RTError(
+                    other.pos_start, other.pos_end, "Division by zero!"
+                )
+            return Number(self.value / other.value), None  
+    
+    def __repr__(self):
+        return str(self.value)
+#################################
+# ------------------ INTERPRETER 
+##################################
+class Interpreter:
+    def visit(self, node):
+        method_name= f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node)
+    
+    def no_visit_method(self, node):
+        raise Exception(f'No visit_{type(node).__name__} method defined!')
+
+###############################################################
+
+    def visit_NumberNode(self, node):
+        return RTResult().success(
+            Number(node.tok.value).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_BinOpNode(self, node):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node))
+        if res.error: 
+            return res
+        right = res.register(self.visit(node.right_node))  
+        if res.error: 
+            return res
+        
+        if  node.op_tok.type ==TT_PLUS:
+            result, error = left.added_to(right)
+        elif  node.op_tok.type ==TT_MINUS:
+            result, error = left.subtracted_by(right)
+        elif  node.op_tok.type ==TT_MUL:
+            result, error = left.multed_by(right)
+        elif  node.op_tok.type ==TT_DIV:
+            result, error = left.dived_by(right)
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    def visit_UnaryOpNode(self, node):
+        res = RTResult()
+        number = res.register(self.visit(node.node))  
+        if res.error: 
+            return res
+        
+        error = None
+
+        if node.op_tok.type == TT_MINUS:
+            number, error = number.multed_by(Number(-1))
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(number.set_pos(node.pos_start, node.pos_end))
 #################################
 # ------------------ RUN 
 ##################################
@@ -317,5 +447,10 @@ def run(fn, text):
 # Generate Abtract Syntax Tree
     parser = Parser(tokens)
     ast = parser.parse()
-
-    return ast.node, ast.error
+    if ast.error:
+        return None, ast.error
+    
+    #run program
+    interpreter = Interpreter()
+    result = interpreter.visit(ast.node)
+    return result.value, result.error
