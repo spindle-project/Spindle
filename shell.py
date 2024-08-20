098ffd43df33b01c82b7b78ea91657c1d11af191
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-08-19 20:37:19 -0500

    Update shell.py
    Made response fancier and padded while also adding a welcome statement

--- shell.py
+++ shell.py
@@ -1,8 +1,9 @@
 import sparkle
+print("Welcome! \nYou now programming with Sparkle ✨ \n")
 while True:
-  text = input("Sparkle > ")
-  result, error = sparkle.run('<placeholder>' , text)
-  if error:
-    print(error.as_string())
-  else:
-    print(result)
+    text = input('✨ | Sparkle > ')
+    result,error = sparkle.run('<stdin>', text)
+    if error: 
+        print(error.as_string())
+    else:
+        print("\n"+str(result)+"\n")
