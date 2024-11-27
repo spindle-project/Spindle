12c6140ea2f9ef8badea0ed60a52542c899f30c7
Shiba-2439 <matthewlozier580@gmail.com>
2024-11-26 19:48:29 -0600

    multiline support almost added. procedures broken for multiline. Everything else working.

--- shell.py
+++ shell.py
@@ -2,6 +2,8 @@ import sparkle
 print("Welcome! \nYou now programming with Sparkle ✨ \n")
 while True:
     text = input('✨ | Sparkle > ')
+    if text.strip() == "":
+        continue
     result,error = sparkle.run('<stdin>', text)
     if error: 
         print(error.as_string())
