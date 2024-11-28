e38091927c30d49f3bfd839ca6e6632b91cdfed8
Shiba-2439 <matthewlozier580@gmail.com>
2024-11-28 12:34:45 -0600

    Removed extraneous files.

--- sparkle.py
+++ sparkle.py
@@ -2,13 +2,39 @@
 # IMPORTS 
 #######################################
 
-
-from strings_with_arrows import *
-
 import string
 import os
 import math
 
+#######################################
+# This function provides fancy error arrows when an error happens
+#######################################
+def string_with_arrows(text, pos_start, pos_end):
+    result = ''
+
+    # Calculate indices
+    idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
+    idx_end = text.find('\n', idx_start + 1)
+    if idx_end < 0: idx_end = len(text)
+    
+    # Generate each line
+    line_count = pos_end.ln - pos_start.ln + 1
+    for i in range(line_count):
+        # Calculate line columns
+        line = text[idx_start:idx_end]
+        col_start = pos_start.col if i == 0 else 0
+        col_end = pos_end.col if i == line_count - 1 else len(line) - 1
+
+        # Append to result
+        result += line + '\n'
+        result += ' ' * col_start + '^' * (col_end - col_start)
+
+        # Re-calculate indices
+        idx_start = idx_end
+        idx_end = text.find('\n', idx_start + 1)
+        if idx_end < 0: idx_end = len(text)
+
+    return result.replace('\t', '')
 
 #######################################
 # CONSTANTS
@@ -1281,7 +1307,7 @@ class Parser:
 		if self.current_tok.type != TT_NEWLINE:
 			return res.failure(InvalidSyntaxError(
 				self.current_tok.pos_start, self.current_tok.pos_end,
-				f"Expected '->' or NEWLINE"
+				"Expected '{' or NEWLINE"
 			))
 
 		res.register_advancement()
