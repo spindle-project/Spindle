698b771160ca4341b3ce00150c8289d1557af7ab
Shiba-2439 <matthewlozier580@gmail.com>
2024-11-28 12:41:59 -0600

    Added a handy guide for how to run a program. Hopefully this will be simplified for the end user.

--- shell.py
+++ shell.py
@@ -1,3 +1,10 @@
+# This is the shell, to run any sparkle program:
+# 1. Ensure you have sparkle.py in the same directory
+# 2. Run shell.py
+# 3. Type "RUN(" + [the file you want to run with the .spkl extension] + ")"
+#       - Example: RUN("HELLO_SPARKLE.spkl")
+# 4. Press enter, and if everything works correctly, you should get an output!
+
 import sparkle
 print("Welcome! \nYou now programming with Sparkle ✨ \n")
 while True:
