4f0f3fb24eb581a662181c6e0e864ac43cd4f673
Matthew <85137306+matthewl580@users.noreply.github.com>
2024-12-12 23:03:05 -0600

    Update shell.py
    Signed-off-by: Matthew <85137306+matthewl580@users.noreply.github.com>

--- shell.py
+++ shell.py
@@ -1,19 +1,19 @@
-# This is the shell, to run any sparkle program:
-# 1. Ensure you have sparkle.py in the same directory
+# This is the shell, to run any spindle program:
+# 1. Ensure you have spindle.py in the same directory
 # 2. Run shell.py
 # 3. Type "RUN(" + [the file you want to run with the .spkl extension] + ")"
-#       - Example: RUN("HELLO_SPARKLE.spkl")
+#       - Example: RUN("HELLO_spindle.spkl")
 # 4. Press enter, and if everything works correctly, you should get an output!
 
-import sparkle
+import spindle
 
 
-print("Welcome! \nYou now programming with Sparkle ✨ \n")
+print("Welcome! \nYou now programming with spindle ✨ \n")
 while True:
-    text = input('✨ | Sparkle > ')
+    text = input('✨ | spindle > ')
     if text.strip() == "":
         continue
-    result = sparkle.run('<stdin>', text)
+    result = spindle.run('<stdin>', text)
     print("\n")
     print(str(repr(result)).replace("None",""))
     
