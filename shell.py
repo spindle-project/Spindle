5f61ee6a81580f35b41aa8fdc512364ccbd44144
Matthew <matthewlozier580@gmail.com>
2025-08-06 22:13:55 -0500

    Update shell.py
    Signed-off-by: Matthew <matthewlozier580@gmail.com>

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 # This is the shell, to run any spindle program:
 # 1. Ensure you have spindle.py in the same directory
 # 2. Run shell.py
-# 3. Type "RUN(" + [the file you want to run with the .spkl extension] + ")"
-#       - Example: RUN("HELLO_spindle.spkl")
+# 3. Type "RUN(" + [the file you want to run with the .spdl extension] + ")"
+#       - Example: RUN("HELLO_spindle.spdl")
 # 4. Press enter, and if everything works correctly, you should get an output!
 
 import spindle
@@ -10,7 +10,7 @@ import spindle
 
 print("Welcome! \nYou now programming with spindle ✨ \n")
 while True:
-    text = input('✨ | spindle > ')
+    text = input('✨ | Spindle > ')
     if text.strip() == "":
         continue
     result = spindle.run('<stdin>', text)
